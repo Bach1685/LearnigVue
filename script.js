ee130b7506ac9be8bac0423302ee168a9849d0b1
Alex <AlexMtkhn@yandex.tu>
2022-06-06 18:48:32 +0300

    список

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 const app = {
     data(){
         return {
-            counter: 0
+            placeHolder: '1',
+            posts: [],
+            currentId: 0,
         }
     },
     methods:{
-        inc(){
-            this.counter++;
+        add(){
+            this.posts.push({id: this.currentId, value: this.placeHolder});
+            this.currentId++;
         },
-        dec(){
-            this.counter--;
-        }
+        remove(_id){
+            let index = this.posts.findIndex((post) => post.id === _id)
+            this.posts.splice(index, 1);
+        },
     }
 }
 
